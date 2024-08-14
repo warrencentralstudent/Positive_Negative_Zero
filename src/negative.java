c5ea90733f020eb265731b7e4e42ae34dfef8398
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-14 12:29:35 -0400

    ?

--- src/negative.java
+++ src/negative.java
@@ -1,8 +1,9 @@
 public class negative {
     public static void main(String[] args) {
+
     }
 
-    public static int checkNumber(int number) {
+    public static void checkNumber(int number) {
         if (number > 0) {
             System.out.println("positive");
         } else if (number < 0) {
